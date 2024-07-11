be10cdea66881b4d8c4dce5482f4100df7c6d9ea
Olya-Kravchuk <143799228+Olya-Kravchuk@users.noreply.github.com>
2024-07-11 20:51:20 +0200

    deliteTag

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,12 @@
-import SharedLayout from "../../components/SharedLayout/SharedLayout";
 import WelcomeSection from "../../components/WelcomeSection/WelcomeSection";
 import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
 
 const HomePage = () => {
   return (
-    <SharedLayout>
+    <>
       <WelcomeSection />
       <AdvantagesSection />
-    </SharedLayout>
+    </>
   );
 };
 
