ffcc7298fd3bf249b3099caaef5b0bb231962b63
Olya-Kravchuk <143799228+Olya-Kravchuk@users.noreply.github.com>
2024-07-11 11:03:43 +0200

    pageBasis

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -1,5 +1,31 @@
+import { Link } from "react-router-dom";
+import clsx from "clsx";
+import Logo from "../Logo/Logo";
+import css from "./WelcomeSection.module.css";
+
 const WelcomeSection = () => {
-  return <div>WelcomeSection</div>;
+  return (
+    <div className={css.welcomeSection}>
+      <Logo />
+      <div className={css.welcomeSectionContainer}>
+        <p className={css.welcomeSectionDescribe}>
+          Record daily water intake and track
+        </p>
+        <h1 className={css.welcomeSectionTitle}>Water consumption tracker</h1>
+        <div className={css.welcomeSectionLinks}>
+          <Link to={`/signup`} className={css.welcomeSectionLink}>
+            Try tracker
+          </Link>
+          <Link
+            to={`/signin`}
+            className={`${css.welcomeSectionLink} ${css.welcomeSectionLinkSignin}`}
+          >
+            Sign In
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WelcomeSection;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,14 @@
+import SharedLayout from "../../components/SharedLayout/SharedLayout";
+import WelcomeSection from "../../components/WelcomeSection/WelcomeSection";
+import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <SharedLayout>
+      <WelcomeSection />
+      <AdvantagesSection />
+    </SharedLayout>
+  );
 };
 
 export default HomePage;
