b64dca6f3619facd100312b8312aa645f30738b8
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-07-12 15:54:22 +0300

    added useModal

--- src/components/ModalDelete/ModalDelete.jsx
+++ src/components/ModalDelete/ModalDelete.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
+import { ANIMATION } from '../../constants.js';
 import css from './ModalDelete.module.css';
 
-const ModalDelete = ({}) => {
-    // const { modal, handleSetModal } = useModal;
+const ModalDelete = ({onClose}) => {
+    const [closing, setClosing] = useState(false);
+
+    const handleClose = () => {
+        setClosing(true);
+        const id = setTimeout(() => {
+            onClose();
+            clearTimeout(id);
+        }, ANIMATION.DURATION);
+    };
 
     return (
         <div className={css.modal}>
-            <button className={css.closeBtn}>x</button>
+            <button type="button" onClick={handleClose} className={css.closeBtn}>
+                <svg>
+                    <use href="src\assets\icons.svg#icon-clear"></use>
+                </svg>
+            </button>
             <div className={css.modalTextBox}>
                 <h2 className={css.modalTitle}>Delete entry</h2>
                 <p className={css.modalText}>Are you sure you want to delete the entry?</p>
@@ -19,3 +33,35 @@ const ModalDelete = ({}) => {
 };
 
 export default ModalDelete;
+
+/* in SomeModalWindowWithoutBackdrop
+
+import { useState } from 'react';
+import { ANIMATION } from "../../constants.js";
+
+const SomeModalWindowWithoutBackdrop = ({ onClose }) => {
+  const [closing, setClosing] = useState(false);
+
+  const handleClose = () => {
+    setClosing(true);
+    const id = setTimeout(() => {
+      onClose();
+      clearTimeout(id);
+    }, ANIMATION.DURATION);
+  };
+
+  return (
+    <div>
+      <h2>Component Title</h2>
+      <p>Some component content</p>
+
+      <button type="button" onClick={handleClose}>
+        CloseModal
+      </button>
+    </div>
+  );
+};
+
+export default SomeModalWindowWithoutBackdrop;
+
+*/
