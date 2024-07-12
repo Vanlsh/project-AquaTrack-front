868bf13aa0831878f79f007ce1f9900d17dcbb3b
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2024-07-12 19:19:10 +0300

    2

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { store, persistor } from "./redux/store.js";
-import { PersistGate } from "redux-persist/integration/react";
-import { Provider } from "react-redux";
-import { ModalProvider } from "./context/ModalProvider.jsx";
-import App from "./components/App.jsx";
-import "modern-normalize";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { store, persistor } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { ModalProvider } from './context/ModalProvider.jsx';
+import App from './components/App.jsx';
+import 'modern-normalize';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <ModalProvider>
-            <App />
-          </ModalProvider>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<PersistGate
+				loading={null}
+				persistor={persistor}>
+				<BrowserRouter>
+					<ModalProvider>
+						<App />
+					</ModalProvider>
+				</BrowserRouter>
+			</PersistGate>
+		</Provider>
+	</React.StrictMode>
 );
