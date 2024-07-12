f485b72fe64c69b705ce8985064e05fbd2be8281
dmvoytyuk <dmvoytyuk@gmail.com>
2024-07-12 22:30:36 +0200

    complete SignInPage + SignInForm
    finished CSS + all media query + validation

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,11 +1,30 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 // @ts-ignore
 import svgSprite from "../../assets/icons.svg";
 import styles from "./SignInForm.module.css";
 
+const schema = yup.object({
+  email: yup
+    .string()
+    .email("Enter a valid email adress!")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(5, "Password is too short")
+    .max(25, "Password is too long")
+    .required("Password is required"),
+});
+
 const SignInForm = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
   const [showPassword, setShowPassword] = useState(false);
 
   const onSubmit = (d) => {
@@ -19,16 +38,23 @@ const SignInForm = () => {
 
   return (
     <form className={styles.signInForm} onSubmit={handleSubmit(onSubmit)}>
-      <h1>Sign In</h1>
+      <h1 className={styles.signInFormTitle}>Sign In</h1>
       <div className={styles.signInFormInputWrapper}>
         <label className={styles.signInFormLabel}>
           Email
           <input
             type="email"
-            className={styles.signInFormInput}
+            className={
+              errors.email?.message
+                ? `${styles.signInFormInput} ${styles.signInFormInputError}`
+                : `${styles.signInFormInput}`
+            }
             {...register("email")}
             placeholder="Enter you email"
           />
+          <p className={styles.signInFormInputErrorMessage}>
+            {errors.email?.message}
+          </p>
         </label>
 
         <label className={styles.signInFormLabel}>
@@ -36,10 +62,15 @@ const SignInForm = () => {
           <div className={styles.signInFormIconInputWrapper}>
             <input
               type={showPassword ? "text" : "password"}
-              className={styles.signInFormInput}
+              className={
+                errors.password?.message
+                  ? `${styles.signInFormInput} ${styles.signInFormInputError}`
+                  : `${styles.signInFormInput}`
+              }
               {...register("password")}
               placeholder="Enter your password"
             />
+
             <button
               className={styles.signInFormIconButton}
               type="button"
@@ -57,6 +88,9 @@ const SignInForm = () => {
               )}
             </button>
           </div>
+          <p className={styles.signInFormInputErrorMessage}>
+            {errors.password?.message}
+          </p>
         </label>
       </div>
       <button className={styles.signInFormButton} type="submit">
--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,17 +1,24 @@
+import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection.jsx";
 import SignFormFooter from "../../components/SignFormFooter/SignFormFooter.jsx";
 import SignFormWrapper from "../../components/SignFromWrapper/SignFormWrapper.jsx";
 import SignInForm from "../../components/SignInForm/SignInForm.jsx";
+import styles from "./SignInPage.module.css";
 
 const SignInPage = () => {
   return (
-    <SignFormWrapper>
-      <SignInForm />
-      <SignFormFooter
-        text="Don't have account? "
-        link="/signup"
-        linkName="Sing Up"
-      />
-    </SignFormWrapper>
+    <div className={styles.signInPage}>
+      <SignFormWrapper>
+        <SignInForm />
+        <SignFormFooter
+          text="Don't have account? "
+          link="/signup"
+          linkName="Sing Up"
+        />
+      </SignFormWrapper>
+      <div className={styles.signInPageRight}>
+        <AdvantagesSection />
+      </div>
+    </div>
   );
 };
 
