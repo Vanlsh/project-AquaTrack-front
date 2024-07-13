d1f6b536b99083aa29bc6dd793edb95d2871e84b
Mykhailo Ulanovych <97468374+UlanovychM@users.noreply.github.com>
2024-07-13 18:43:25 +0300

    update

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -1,5 +1,48 @@
-const AddWaterBtn = () => {
-  return <div>AddWaterBtn</div>;
+import { useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+import css from './AddWaterBtn.module.css';
+
+const AddWaterBtn = (isBig, selectedDate) => {
+	const [modIsOpen, setModIsOpen] = useState(false);
+	const [basicModIsOpen, setBasicModIsOpen] = useState(false);
+	const [success, setSuccess] = useState(false);
+
+	const dispatch = useDispatch();
+
+	const openAddModal = () => {
+		setModIsOpen(true);
+		setBasicModIsOpen(true);
+	};
+
+	const closeWaterModal = () => {
+		setModIsOpen(false);
+		setBasicModIsOpen(false);
+	};
+
+	const handleSubmit = async data => {};
+
+	return success ? (
+		<Toaster position='top-center' />
+	) : (
+		<>
+			<div className={css.addBtnWrap}>
+				<button
+					className={isBig ? css.btnBig : css.btnSmall}
+					type='button'
+					onClick={openAddModal}
+				>
+					{isBig ? (
+						{
+							/* icon */
+						}
+					) : (
+						<div className={css.iconWrapper}>{/* icon */}</div>
+					)}
+					<span className={isBig ? css.descSmall : css.descBig}>Add water</span>
+				</button>
+			</div>
+		</>
+	);
 };
 
 export default AddWaterBtn;
