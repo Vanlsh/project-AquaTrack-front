e10da8618f89d46e8ab06428fd9514d0f7fb8387
Olena-Ishchenko <elena.dubovalova@gmail.com>
2024-07-13 23:20:30 +0300

    add advantagesection and update input

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,17 +1,24 @@
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import SignFormWrapper from "../../components/SignFromWrapper/SignFormWrapper";
 import SignFormFooter from "../../components/SignFormFooter/SignFormFooter";
+import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
+import styles from "./SignUpPage.module.css";
 
 const SignUpPage = () => {
   return (
-    <SignFormWrapper>
-      <SignUpForm />
-      <SignFormFooter
-        text="Already have account? "
-        link="/signin"
-        linkName="Sing In"
-      />
-    </SignFormWrapper>
+    <div className={styles.signUpPage}>
+      <SignFormWrapper>
+        <SignUpForm />
+        <SignFormFooter
+          text="Already have account? "
+          link="/signin"
+          linkName="Sing In"
+        />
+      </SignFormWrapper>
+      <div className={styles.signUpPageRight}>
+        <AdvantagesSection />
+      </div>
+    </div>
   );
 };
 
