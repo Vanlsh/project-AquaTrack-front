202914242e8f683203bd48b91e7277556057afad
dmvoytyuk <dmvoytyuk@gmail.com>
2024-07-14 12:06:32 +0200

    TrackerPage + userBar + userPanel rename + avatar placeholder

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,9 +1,11 @@
+import WaterDetailedInfo from "../../components/WaterDetailedInfo/WaterDetailedInfo.jsx";
 import WaterMainInfo from "../../components/WaterMainInfo/WaterMainInfo";
 
 const TrackerPage = () => {
   return (
     <>
       <WaterMainInfo />
+      <WaterDetailedInfo />
     </>
   );
 };
