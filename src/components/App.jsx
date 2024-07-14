2e16058560bf0120055519cd11394c6af9d18b78
Serhii Antonets <antonets.serhii.ua@gmail.com>
2024-07-14 18:53:14 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import SharedLayout from "./SharedLayout/SharedLayout.jsx";
-import SomeComponent from "./WraperWaterModal/WraperWaterModal.jsx";
 
 const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
 const SignInPage = lazy(() => import("../pages/SignInPage/SignInPage.jsx"));
