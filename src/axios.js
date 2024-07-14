f343e5574c916888f23be9636763f260067e1eef
Mykhailo <matsyshynmykhailo@gmail.com>
2024-07-14 12:26:01 +0300

    fix url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://project-aquatrack-back.onrender.com",
 });
 
 export const setAuthHeader = (token) => {
