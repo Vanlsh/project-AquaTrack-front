a81b9421c25dcbeb2ed4471b3856e6b05ea5b214
Mykhailo <matsyshynmykhailo@gmail.com>
2024-07-15 12:38:47 +0300

    merge

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,10 +1,9 @@
 import WaterDetailedInfo from "../../components/WaterDetailedInfo/WaterDetailedInfo.jsx";
 import WaterMainInfo from "../../components/WaterMainInfo/WaterMainInfo";
-import TestButton from "../../components/TestButton"
+
 const TrackerPage = () => {
   return (
     <>
-      <TestButton/>
       <WaterMainInfo />
       <WaterDetailedInfo />
     </>
