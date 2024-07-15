6d39a94c376ef7b8184881ffa81280ff39e600a4
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-07-15 22:26:36 +0300

    delete test file

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,11 @@
 import WelcomeSection from "../../components/WelcomeSection/WelcomeSection";
 import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
-import SomeComponent from "../../components/Test/Test.jsx";
 
 const HomePage = () => {
   return (
     <>
       <WelcomeSection />
       <AdvantagesSection />
-      <SomeComponent/>
     </>
   );
 };
