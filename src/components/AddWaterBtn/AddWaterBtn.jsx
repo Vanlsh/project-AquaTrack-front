4ffa4d64142cb8c46b7efd36d1dee2fc3e53651a
Mykhailo Ulanovych <97468374+UlanovychM@users.noreply.github.com>
2024-07-16 12:25:04 +0300

    update

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -1,6 +1,50 @@
+import css from '../AddWaterBtn/AddWaterBtn.module.css';
+import WaterModal from '../../components/WaterModal/WaterModal';
+import { useState } from 'react';
+import clsx from 'clsx';
+import Modal from '../Modal/Modal';
 
-const AddWaterBtn = () => {
-	
+const AddWaterBtn = ({ isOpen }) => {
+	const [isOpenModal, setIsOpenModal] = useState(false);
+
+	const handleOpenModal = () => {
+		setIsOpenModal(true);
+	};
+
+	return (
+		<>
+			<button
+				className={clsx(css.btn, isOpen && css.btnStyle)}
+				type='button'
+				onClick={() => {
+					handleOpenModal();
+				}}
+			>
+				<svg
+					className={clsx(css.icon, isOpen && css.iconStyle)}
+					width='16'
+					height='16'
+				>
+					<use href='src\assets\icons.svg#icon-clear'></use>
+				</svg>
+				Add water
+			</button>
+			{isOpenModal && (
+				<Modal
+					isOpen={isOpenModal}
+					onClose={() => {
+						setIsOpenModal(false);
+					}}
+				>
+					<WaterModal
+						operationAdd={true}
+						operationType='add'
+						isOpen={setIsOpenModal}
+					/>
+				</Modal>
+			)}
+		</>
+	);
 };
 
 export default AddWaterBtn;
--- src/components/DailyInfo/DailyInfo.jsx
+++ src/components/DailyInfo/DailyInfo.jsx
@@ -1,27 +1,29 @@
-import WaterList from "../WaterList/WaterList";
-import css from "./DailyInfo.module.css";
-import AddWaterBtn from "../AddWaterBtn/AddWaterBtn";
-import ChooseDate from "../ChooseDate/ChooseDate";
-// import { useState } from 'react';
+import WaterList from '../WaterList/WaterList';
+import css from './DailyInfo.module.css';
+import AddWaterBtn from '../AddWaterBtn/AddWaterBtn';
+import ChooseDate from '../ChooseDate/ChooseDate';
+import { useState } from 'react';
+import Modal from '../Modal/Modal';
+import WaterModal from '../WaterModal/WaterModal';
 
 const DailyInfo = () => {
-  // const [isOpen, setIsOpen] = useState(false);
-  return (
-    <div className={css.info}>
-      <div className={css.header}>
-        <ChooseDate />
-        <AddWaterBtn />
-      </div>
-      {/* {isOpen && (
-        <Modal>
-          <WaterModal />
-        </Modal>
-      )} */}
-      <div className={css.content}>
-        <WaterList />
-      </div>
-    </div>
-  );
+	const [isOpen, setIsOpen] = useState(false);
+	return (
+		<div className={css.info}>
+			<div className={css.header}>
+				<ChooseDate />
+				<AddWaterBtn isOpen={true} />
+			</div>
+			{isOpen && (
+				<Modal>
+					<WaterModal />
+				</Modal>
+			)}
+			<div className={css.content}>
+				<WaterList />
+			</div>
+		</div>
+	);
 };
 
 export default DailyInfo;
