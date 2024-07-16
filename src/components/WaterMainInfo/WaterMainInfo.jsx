e7ec3f90b5e2f49cdc874d3205a9e374e33f0668
VladyslavHn <vlad.gnatok@gmail.com>
2024-07-16 22:49:43 +0200

    not finish

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -5,10 +5,17 @@ import css from "./WaterMainInfo.module.css";
 import { useModal } from "../../hooks/useModal.js";
 import svgSprite from "../../assets/icons.svg";
 import WaterModal from "../WaterModal/WaterModal.jsx";
+import UserSettingsModal from "../UserSettingsModal/UserSettingsModal.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { selectWaterDailyAmount, selectWaterDailyPercentage, selectWaterDailyRecord, selectWaterMonthlyRecord } from "../../redux/water/selectors.js";
+import { fetchDailyWater, fetchMonthlyWater } from "../../redux/water/operations.js";
+import Logo from "../Logo/Logo.jsx";
 
 const WaterMainInfo = () => {
-  const [goal, setGoal] = useState(0);
+  const dispatch = useDispatch();
+  const dailyWaterAmount = useSelector(selectWaterDailyRecord)
   const setModal = useModal();
+  console.log(dailyWaterAmount);
 
   const closeModal = useCallback(() => {
     setModal();
@@ -19,51 +26,41 @@ const WaterMainInfo = () => {
   }, [setModal, closeModal]);
 
     const setGoalModal = useCallback(() => {
-    setModal(<WaterModal onClose={closeModal} />);
+    setModal(<UserSettingsModal  onClose={closeModal} />);
   }, [setModal, closeModal]);
 
-  // useEffect(() => {
-  //   const getGoal = async () => {
-  //     try {
-  //       const response = await fetch('');
-  //       const data = await response.json();
-  //       setGoal(data.goal)
-  //     } catch (error) {
-  //       console.error('Помилка при отриманні даних:', error);
-  //     }
-  //   }
-  //   getGoal();
-  // }, [])
+  useEffect(() => {
+    dispatch(fetchMonthlyWater())
+  }, [dispatch])
 
   return (
-      <div className={css.waterContainer}>
-      <h1 className={css.waterTitle}>AquaTrack</h1>
-      {/* {goal === 0 ? <div className={css.normaContainer} style={{display: none}} /> : <div className={css.normaContainer}>} */}
+    <div className={css.waterContainer}>
+      <Logo className={css.waterTitle}/>
+      
+      {dailyWaterAmount === 0 ? (<div className={css.normaContainer} style={{ display: 'none' }} />
+      ) : (
         <div className={css.normaContainer}>
-          <h2 className={css.normaL}>{goal === 0 ? 'You don’t have a goal yet!' : `${goal} L`}</h2>
+            <h2 className={css.normaL}>{`${dailyWaterAmount} L`}</h2>
           <p className={css.normaText}>My daily norma</p>
-      </div>
-      {/* {goal === 0 ? <WaterProgressBar style={{display: none}} /> : <WaterProgressBar />} */}
-      <WaterProgressBar />
-      {/* {goal === 0 ? <button type="button" className={css.btnAdd}  onClick={openModal}>
+        </div>)}
+      {dailyWaterAmount === 0 ? ('')
+        :
+        (<WaterProgressBar />)}
+      
+       {dailyWaterAmount === 0 ? (<button type="button" className={css.btnSet}  onClick={setGoalModal}>
           <svg className={css.plus}>
             <use xlinkHref={svgSprite + "#icon-plus"} />
           </svg>
-          <h2 className={css.btnText}>Add water</h2>
-        </button>
-          :
-          <button type="button" className={css.btnAdd}  onClick={openModal}>
-          <svg className={css.plus}>
-            <use xlinkHref={svgSprite + "#icon-plus"} />
-          </svg>
-          <h2 className={css.btnText}>Add water</h2>
-        </button> */}
+          <h2 className={css.btnTextSet}>Set your norm</h2>
+        </button>)
+          :(
+         
         <button type="button" className={css.btnAdd} onClick={openModal}>
           <svg className={css.plus}>
             <use xlinkHref={svgSprite + "#icon-plus"} />
           </svg>
           <h2 className={css.btnText}>Add water</h2>
-        </button>
+        </button>)}
       </div>
     
   );
