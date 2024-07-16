59c441bfb4b57ff2cb55b6ddb83b289138e81ca8
Mykhailo Ulanovych <97468374+UlanovychM@users.noreply.github.com>
2024-07-16 11:02:30 +0300

    update

--- src/components/ChooseDate/ChooseDate.jsx
+++ src/components/ChooseDate/ChooseDate.jsx
@@ -1,12 +1,23 @@
 import css from './ChooseDate.module.css';
+import { useEffect, useState } from 'react';
 
-const ChooseDate = ({ selectedDate }) => {
+const ChooseDate = () => {
+	const [date, setDate] = useState("");
+
+  useEffect(() => {
+
+    const today = new Date().toDateString();
+
+
+    if (today === new Date().toDateString()) {
+      setDate(`Today`);
+    }
+
+  },[]);
 	return (
 		<div className={css.wrap}>
 			<h3 className={css.selectedDate}>
-				{/* {data === date.full
-					? t(today)
-					: `${day}, ${t(month} */}
+				{date}
 			</h3>
 		</div>
 	);
--- src/components/DailyInfo/DailyInfo.jsx
+++ src/components/DailyInfo/DailyInfo.jsx
@@ -2,16 +2,23 @@ import WaterList from '../WaterList/WaterList';
 import css from './DailyInfo.module.css';
 import AddWaterBtn from '../AddWaterBtn/AddWaterBtn';
 import ChooseDate from '../ChooseDate/ChooseDate';
+// import { useState } from 'react';
 
-const DailyInfo = ({ selectedDate }) => {
+const DailyInfo = () => {
+	// const [isOpen, setIsOpen] = useState(false);
 	return (
 		<div className={css.info}>
 			<div className={css.header}>
-				<ChooseDate selectedDate={selectedDate} />
-				<AddWaterBtn isBig={false} selectedDate={selectedDate} />
+				<ChooseDate  />
+				<AddWaterBtn  />
 			</div>
+			{/* {isOpen && (
+        <Modal>
+          <WaterModal />
+        </Modal>
+      )} */}
 			<div className={css.content}>
-				<WaterList selectedDate={selectedDate} />
+				<WaterList />
 			</div>
 		</div>
 	);
