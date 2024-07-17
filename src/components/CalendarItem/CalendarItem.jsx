ac3ebdbcb7da046b987b7c0e93cde39f65a422b2
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2024-07-18 01:38:15 +0300

    loading

--- src/components/CalendarItem/CalendarItem.jsx
+++ src/components/CalendarItem/CalendarItem.jsx
@@ -1,41 +1,43 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-import css from './CalendarItem.module.css';
+import css from "./CalendarItem.module.css";
 
 const CalendarItem = ({
-	index,
-	calendarDate,
-	percent,
-	selectedIndex,
-	setSelectedIndex,
+  index,
+  calendarDate,
+  percent,
+  selectedIndex,
+  setSelectedIndex,
 }) => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	// Thunk for data for selected month;
+  // Thunk for data for selected month;
 
-	const handleClick = (index, calendarDate) => {
-		setSelectedIndex(index);
-		navigate(`/tracker/${calendarDate}`);
-		// Thunk for data for selected date;
-	};
+  const handleClick = (index, calendarDate) => {
+    setSelectedIndex(index);
+    navigate(`/tracker/${calendarDate}`);
+    // тут не потрібна санка
+  };
 
-	const date = new Date(calendarDate).getDate();
-	const dateNow = Date.now();
-	const isDisabled = calendarDate > dateNow;
+  const date = new Date(Number(calendarDate)).getDate();
+  const dateNow = Date.now();
+  const isDisabled = Number(calendarDate) > dateNow;
 
-	return (
-		<button
-			className={`${css.day} ${isDisabled ? css.disabled : ''}`}
-			disabled={isDisabled}>
-			<div
-				onClick={() => handleClick(index, calendarDate)}
-				className={`${css.date} 							
-					${percent > 0 ? css.perc_filled : ''} ${selectedIndex === index ? css.active : ''}`}>
-				{date}
-			</div>
-			<div className={css.perc}>{`${percent} %`}</div>
-		</button>
-	);
+  return (
+    <button
+      className={`${css.day} ${isDisabled ? css.disabled : ""}`}
+      disabled={isDisabled}
+    >
+      <div
+        onClick={() => handleClick(index, calendarDate)}
+        className={`${css.date} 							
+					${percent > 0 ? css.perc_filled : ""} ${selectedIndex === index ? css.active : ""}`}
+      >
+        {date}
+      </div>
+      <div className={css.perc}>{`${percent}%`}</div>
+    </button>
+  );
 };
 
 export default CalendarItem;
--- src/components/CalendarPagination/CalendarPagination.jsx
+++ src/components/CalendarPagination/CalendarPagination.jsx
@@ -1,78 +1,71 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
-import { Title } from '../Title/Title.jsx';
-import { parseDateTime } from '../../helpers/parseDate.js';
-import svg from '../../assets/icons.svg';
-import { monthsName } from '../../constants';
-import css from './CalendarPagination.module.css';
+import { Title } from "../Title/Title.jsx";
+import { parseDateTime } from "../../helpers/parseDate.js";
+import svg from "../../assets/icons.svg";
+import { monthsName } from "../../constants";
+import css from "./CalendarPagination.module.css";
 
 const CalendarPagination = ({ setSelectedIndex }) => {
-	const { dateUrl } = useParams();
-	const dateMs = parseDateTime(dateUrl);
+  const { dateUrl } = useParams();
+  const dateMs = parseDateTime(dateUrl);
 
-	const [year, setYear] = useState(new Date(dateMs).getFullYear());
-	const [month, setMonth] = useState(new Date(dateMs).getMonth());
+  const [year, setYear] = useState(new Date(dateMs).getFullYear());
+  const [month, setMonth] = useState(new Date(dateMs).getMonth());
 
-	// Thunk for data for selected month
+  // Thunk for data for selected month
 
-	const increment = () => {
-		setSelectedIndex(null);
-		if (month === 11) {
-			setMonth(0);
-			setYear(year + 1);
-			return;
-		}
-		setMonth(month + 1);
-		// Thunk
-	};
+  const increment = () => {
+    setSelectedIndex(null);
+    if (month === 11) {
+      setMonth(0);
+      setYear(year + 1);
+      return;
+    }
+    setMonth(month + 1);
+    // Thunk
+  };
 
-	const decrement = () => {
-		setSelectedIndex(null);
-		if (month === 0) {
-			setMonth(11);
-			setYear(year - 1);
-			return;
-		}
-		setMonth(month - 1);
-		// Thunk
-	};
+  const decrement = () => {
+    setSelectedIndex(null);
+    if (month === 0) {
+      setMonth(11);
+      setYear(year - 1);
+      return;
+    }
+    setMonth(month - 1);
+    // Thunk
+  };
 
-	const selectedMonth = monthsName[month];
+  const selectedMonth = monthsName[month];
 
-	return (
-		<div className={css.calendar_title}>
-			<Title
-				title={'Month'}
-				styles={css.month}
-			/>
-			<div className={css.month_ind}>
-				<button
-					onClick={decrement}
-					className={css.btn}>
-					<svg className={css.svg_arrow_left}>
-						<use xlinkHref={svg + '#icon-arrow'}></use>
-					</svg>
-				</button>
-				<span className={css.month_year}>
-					{`${selectedMonth},
+  return (
+    <div className={css.calendar_title}>
+      <Title title={"Month"} styles={css.month} />
+      <div className={css.month_ind}>
+        <button onClick={decrement} className={css.btn}>
+          <svg className={css.svg_arrow_left}>
+            <use xlinkHref={svg + "#icon-arrow"}></use>
+          </svg>
+        </button>
+        <span className={css.month_year}>
+          {`${selectedMonth},
 					${year}`}
-				</span>
-				<button
-					onClick={increment}
-					className={css.btn}>
-					<svg className={css.svg_arrow_right}>
-						<use xlinkHref={svg + '#icon-arrow'}></use>
-					</svg>
-				</button>
-				<div className={css.statistic_btn}>
-					<svg className={css.svg_pie}>
-						<use xlinkHref={svg + '#icon-pie-chart'}></use>
-					</svg>
-				</div>
-			</div>
-		</div>
-	);
+        </span>
+        <button onClick={increment} className={css.btn}>
+          <svg className={css.svg_arrow_right}>
+            <use xlinkHref={svg + "#icon-arrow"}></use>
+          </svg>
+        </button>
+        <div className={css.statistic_btn}>
+          <svg className={css.svg_pie}>
+            <use xlinkHref={svg + "#icon-pie-chart"}></use>
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CalendarPagination;
