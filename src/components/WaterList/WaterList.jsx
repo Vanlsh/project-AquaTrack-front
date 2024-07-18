a6f542c4cbdd37d23e8ce8bc4110c935e40e771b
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2024-07-18 19:06:25 +0300

    add skeleton

--- src/components/WaterList/WaterList.jsx
+++ src/components/WaterList/WaterList.jsx
@@ -1,10 +1,27 @@
 import { useSelector } from "react-redux";
 import WaterItem from "../WaterItem/WaterItem";
 import css from "./WaterList.module.css";
-import { selectWaterDailyRecord } from "../../redux/water/selectors.js";
+import {
+  selectWaterDailyRecord,
+  selectDailyIsLoading,
+} from "../../redux/water/selectors.js";
+import { useEffect } from "react";
 
 const WaterList = () => {
   const data = useSelector(selectWaterDailyRecord);
+  const isLoading = useSelector(selectDailyIsLoading);
+  useEffect(() => {
+    console.log(isLoading);
+  }, [isLoading]);
+
+  if (isLoading)
+    return (
+      <div className={css.waterList}>
+        {[1, 2, 3].map((item) => (
+          <div key={item} className={css.skeleton}></div>
+        ))}
+      </div>
+    );
   return (
     <ul className={css.waterList}>
       {data.map((item) => (
