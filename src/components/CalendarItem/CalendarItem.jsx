909cf8c247575b1aeb9a43ce49a812307a99eced
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2024-07-18 21:22:31 +0300

    fix styles

--- src/components/CalendarItem/CalendarItem.jsx
+++ src/components/CalendarItem/CalendarItem.jsx
@@ -1,42 +1,57 @@
-import { useNavigate } from 'react-router-dom';
-import css from './CalendarItem.module.css';
-import { useDispatch } from 'react-redux';
-import { fetchDailyWater } from '../../redux/water/operations';
+import { useNavigate } from "react-router-dom";
+import css from "./CalendarItem.module.css";
+import { useDispatch } from "react-redux";
+import { fetchDailyWater } from "../../redux/water/operations";
+import { useParams } from "react-router-dom";
+import { parseDateTime } from "../../helpers/parseDate.js";
+import clsx from "clsx";
 
-const CalendarItem = ({
-	index,
-	calendarDate,
-	percent,
-	selectedIndex,
-	setSelectedIndex,
-}) => {
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
+const isDaySame = (firstDay, secondDay) => {
+  const first = new Date(Number(firstDay));
+  const second = new Date(Number(secondDay));
+  return (
+    first.getFullYear() === second.getFullYear() &&
+    first.getMonth() === second.getMonth() &&
+    first.getDate() === second.getDate()
+  );
+};
 
-	const handleClick = (index, calendarDate) => {
-		setSelectedIndex(index);
-		navigate(`/tracker/${calendarDate}`);
-		dispatch(fetchDailyWater(calendarDate));
-	};
+const CalendarItem = ({ calendarDate, percent }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-	const date = new Date(Number(calendarDate)).getDate();
-	const dateNow = Date.now();
-	const isDisabled = Number(calendarDate) > dateNow;
+  const { date: paramsDate } = useParams();
+  const currentDate = parseDateTime(paramsDate);
 
-	return (
-		<button
-			className={`${css.day} ${isDisabled ? css.disabled : ''} 
-      ${!isDisabled ? css.pointer_enabled : css.pointer_disabled}`}
-			disabled={isDisabled}>
-			<div
-				onClick={() => handleClick(index, calendarDate)}
-				className={`${css.date} 							
-					${Math.round(percent) >= 100 ? css.perc_filled : ''} ${selectedIndex === index ? css.active : ''}`}>
-				{date}
-			</div>
-			<div className={css.perc}>{`${Math.round(percent)}%`}</div>
-		</button>
-	);
+  const handleClick = (calendarDate) => {
+    navigate(`/tracker/${calendarDate}`);
+    dispatch(fetchDailyWater(calendarDate));
+  };
+
+  const date = new Date(Number(calendarDate)).getDate();
+  const dateNow = Date.now();
+  const isDisabled = Number(calendarDate) > dateNow;
+  const isDane = Math.round(percent) < 100;
+  const isActive = isDaySame(currentDate, calendarDate);
+  return (
+    <button
+      className={clsx(css.day, {
+        [css.disabled]: isDisabled,
+      })}
+      disabled={isDisabled}
+      onClick={() => handleClick(calendarDate)}
+    >
+      <div
+        className={clsx(css.date, {
+          [css.perc_filled]: isDane,
+          [css.active]: isActive,
+        })}
+      >
+        {date}
+      </div>
+      <div className={css.perc}>{`${Math.round(percent)}%`}</div>
+    </button>
+  );
 };
 
 export default CalendarItem;
