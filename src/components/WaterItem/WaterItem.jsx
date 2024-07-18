337690cd3810562b8251eff18e7bf281b845ab60
Serhii Antonets <antonets.serhii.ua@gmail.com>
2024-07-18 21:17:05 +0300

    update WaterItem with props

--- src/components/WaterItem/WaterItem.jsx
+++ src/components/WaterItem/WaterItem.jsx
@@ -18,8 +18,10 @@ const WaterItem = ({ water }) => {
   }, [setModal, closeModal, water]);
 
   const openModalEdit = useCallback(() => {
-    setModal(<WaterModal water={water} onClose={closeModal} />);
+    setModal(<WaterModal water={water} onClose={closeModal} operationType={"edit"}/>);
   }, [setModal, closeModal, water]);
+
+console.log(water)
 
   return (
     <div className={css.water_item_content}>
