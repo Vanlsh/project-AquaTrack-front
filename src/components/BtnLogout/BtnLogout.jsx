9df18286ebd61e6353017e47082b3351dd40e70e
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-07-19 18:37:21 +0300

    added Loader and path to BtnLogOut

--- src/components/BtnLogout/BtnLogout.jsx
+++ src/components/BtnLogout/BtnLogout.jsx
@@ -1,11 +1,15 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 import { logOut } from "../../redux/auth/operations";
 import css from "./BtnLogout.module.css";
+import { Link } from "react-router-dom";
+import { selectIsLoading } from "../../redux/auth/selectors.js";
+import LoaderComponent from '../LoaderComponent/LoaderComponent.jsx'
 
 const BtnLogout = ({ handleClose }) => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
 
   const handleLogout = () => {
     dispatch(logOut());
@@ -13,9 +17,11 @@ const BtnLogout = ({ handleClose }) => {
   };
 
   return (
+    <>
     <button type="button" onClick={handleLogout} className={css.btnLogout}>
-      {t("logout")}
+      {isLoading ? <LoaderComponent height={80} width={80} /> : <Link to={'/'}>{t("logout")}</Link>}  
     </button>
+    </>
   );
 };
 
