e903f8618507b8bc56b62222f1900e3bf6b4142a
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-07-19 23:16:27 +0300

    update LogoutModal

--- src/components/BtnLogout/BtnLogout.jsx
+++ src/components/BtnLogout/BtnLogout.jsx
@@ -2,26 +2,27 @@ import { useDispatch } from "react-redux";
 import { useTranslation } from "react-i18next";
 import { logOut } from "../../redux/auth/operations";
 import css from "./BtnLogout.module.css";
-import { Link } from "react-router-dom";
-// import { selectIsLoading } from "../../redux/auth/selectors.js";
-// import LoaderComponent from '../LoaderComponent/LoaderComponent.jsx'
+import { useNavigate } from "react-router-dom";
 
 const BtnLogout = ({ handleClose }) => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
-  // const isLoading = useSelector(selectIsLoading);
+  const navigate = useNavigate();
 
   const handleLogout = () => {
-    dispatch(logOut());
-    handleClose();
+    dispatch(logOut()).then(({error}) => {
+      if (!error) {
+        handleClose();
+        navigate('/');
+      }
+    }); 
   };
 
   return (
     <>
       <button type="button" onClick={handleLogout} className={css.btnLogout}>
-        <Link to={'/'}>{t("logout")}</Link>
-      {/* {isLoading ? <LoaderComponent height={80} width={80} /> : <Link to={'/'}>{t("logout")}</Link>}   */}
-    </button>
+        {t("logout")}
+      </button>
     </>
   );
 };
