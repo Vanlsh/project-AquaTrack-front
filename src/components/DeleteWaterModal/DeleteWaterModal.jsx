6c392cb2f977e8fc0dbff10e6f1331f9dbe51461
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2024-07-20 13:25:28 +0300

    delete log

--- src/components/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/components/DeleteWaterModal/DeleteWaterModal.jsx
@@ -1,46 +1,50 @@
-import svg from '../../assets/icons.svg';
-import css from './DeleteWaterModal.module.css';
-import BtnDelete from '../BtnDelete/BtnDelete.jsx';
-import { ANIMATION } from '../../constants.js';
-import { useTranslation } from 'react-i18next';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { deleteWaterIntakeRecord } from '../../redux/water/operations.js';
-import LoaderComponent from '../LoaderComponent/LoaderComponent.jsx';
+import svg from "../../assets/icons.svg";
+import css from "./DeleteWaterModal.module.css";
+import BtnDelete from "../BtnDelete/BtnDelete.jsx";
+import { ANIMATION } from "../../constants.js";
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { deleteWaterIntakeRecord } from "../../redux/water/operations.js";
+import LoaderComponent from "../LoaderComponent/LoaderComponent.jsx";
 
-const ModalDeleteEntry = ({id, onClose}) => {
-  const { t } = useTranslation(); 
+const ModalDeleteEntry = ({ id, onClose }) => {
+  const { t } = useTranslation();
   const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
-  
+
   const handleClose = () => {
     const id = setTimeout(() => {
       onClose();
       clearTimeout(id);
     }, ANIMATION.DURATION);
   };
-    
-    const handleDelete = () => {
-        setIsLoading(() => true);
-        dispatch(deleteWaterIntakeRecord(id)).then(({ error }) => {
-            if (!error) {
-              handleClose();
-             console.log('handleClose'); 
-          }
-          setIsLoading(false);
-        }); 
-    };
+
+  const handleDelete = () => {
+    setIsLoading(() => true);
+    dispatch(deleteWaterIntakeRecord(id)).then(({ error }) => {
+      if (!error) {
+        handleClose();
+      }
+      setIsLoading(false);
+    });
+  };
 
   return (
     <div className={css.modal}>
-      <button type="button" aria-label='Close the delete entry modal window' onClick={handleClose} className={css.closeBtn}>
-          <svg className={css.svg}>
-            <use xlinkHref={svg + '#icon-x'}></use>
-          </svg>
+      <button
+        type="button"
+        aria-label="Close the delete entry modal window"
+        onClick={handleClose}
+        className={css.closeBtn}
+      >
+        <svg className={css.svg}>
+          <use xlinkHref={svg + "#icon-x"}></use>
+        </svg>
       </button>
       <div className={css.modalTextBox}>
-          <h2 className={css.modalTitle}>{t("deleteEntry")}</h2>
-          <p className={css.modalText}>{t("confirmDeleteEntry")}</p>
+        <h2 className={css.modalTitle}>{t("deleteEntry")}</h2>
+        <p className={css.modalText}>{t("confirmDeleteEntry")}</p>
       </div>
       <div className={css.modalBtnBox}>
         {isLoading ? (
@@ -56,7 +60,7 @@ const ModalDeleteEntry = ({id, onClose}) => {
               {t("cancel")}
             </button>
           </>
-        )} 
+        )}
       </div>
     </div>
   );
