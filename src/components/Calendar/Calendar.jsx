7dce62013cdecff5f71d199c2b3941a008147824
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2024-07-20 13:19:32 +0300

    fix(water): add error check

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -2,17 +2,27 @@ import { useSelector } from "react-redux";
 import {
   selectWaterMonthlyRecord,
   selectMonthlyIsLoading,
+  selectMonthlyIsError,
 } from "../../redux/water/selectors.js";
 import CalendarItem from "../CalendarItem/CalendarItem.jsx";
 import Loader from "../Loader/Loader.jsx";
 
 import css from "./Calendar.module.css";
+import { useTranslation } from "react-i18next";
 
 const Calendar = () => {
+  const { t } = useTranslation();
   const dateArray = useSelector(selectWaterMonthlyRecord);
   const isLoading = useSelector(selectMonthlyIsLoading);
-
+  const isError = useSelector(selectMonthlyIsError);
   if (isLoading) return <Loader />;
+
+  if (isError)
+    return (
+      <div className={css.container}>
+        <h2 className={css.errorMessage}>{t("waterMonthErrorMessage")}</h2>
+      </div>
+    );
   return (
     <div className={css.container}>
       <ul className={css.calendarList}>
